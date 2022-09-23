e97cd398d9840ce9a1d53a12b3715e8388eb380b
edo979 <prezentabilan@gmail.com>
2022-09-23 09:05:33 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+function App() {
+  return (
+    <>
+      <header>
+        <h1>Planets</h1>
+        <nav className="navbar">
+          <ul className="navbar_list">
+            <li className="navbar_item">
+              <a href="#" className="navbar_link">
+                Planet
+              </a>
+              <a href="#" className="navbar_link">
+                Planet
+              </a>
+              <a href="#" className="navbar_link">
+                Planet
+              </a>
+              <a href="#" className="navbar_link">
+                Planet
+              </a>
+              <a href="#" className="navbar_link">
+                Planet
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main></main>
+    </>
+  )
+}
+
+export default App
