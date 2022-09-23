bcc3afc058ec9d8efcf94ce5dcefe82ae4a2c225
edo979 <prezentabilan@gmail.com>
2022-09-23 19:17:46 +0200

    start styling content

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,17 @@ function App() {
       </header>
 
       <Outlet />
+
+      <footer>
+        Photo by{' '}
+        <a href="https://unsplash.com/@nasa?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+          NASA
+        </a>{' '}
+        on{' '}
+        <a href="https://unsplash.com/@nasa?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+          Unsplash
+        </a>
+      </footer>
     </>
   )
 }
--- src/pages/Planet.tsx
+++ src/pages/Planet.tsx
@@ -1,5 +1,28 @@
+import { useParams } from 'react-router-dom'
+
 function Planet() {
-  return <div>Planet</div>
+  const { planet } = useParams()
+
+  return (
+    <main>
+      <section className="content | flex">
+        <aside className="content_image">
+          <img src="assets/images/earth.jpg" alt={planet} />
+        </aside>
+
+        <article className="content_text-container">
+          <h2 className="content_title">{planet}</h2>
+          <p className="content_text">
+            Earth is the third planet from the Sun and the only astronomical
+            object known to harbor life. While large volumes of water can be
+            found throughout the Solar System, only Earth sustains liquid
+            surface water. About 71% of Earth's surface is made up of the ocean,
+            dwarfing Earth's polar ice, lakes, and rivers.
+          </p>
+        </article>
+      </section>
+    </main>
+  )
 }
 
 export default Planet
