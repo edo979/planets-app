ede0e90c70c3f3db7db8699f983d268e801e50b7
edo979 <prezentabilan@gmail.com>
2022-09-23 22:43:14 +0200

    add fonts, add spacing, fix width

--- src/pages/Planet.tsx
+++ src/pages/Planet.tsx
@@ -5,7 +5,7 @@ function Planet() {
 
   return (
     <main>
-      <section className="content | flex">
+      <section className="content | flex mt-2">
         <aside className="content_image">
           <img src="assets/images/earth.jpg" alt={planet} />
         </aside>
@@ -22,7 +22,7 @@ function Planet() {
         </article>
       </section>
 
-      <section className="boxes | flex">
+      <section className="boxes | flex mt-2">
         <div className="box skew-box">
           <h3>Age:</h3>
           <p>4.543 billion years</p>
