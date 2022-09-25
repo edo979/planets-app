89d462f6f646aefeaf1e9e68ad28031b1729d510
edo979 <prezentabilan@gmail.com>
2022-09-25 13:40:48 +0200

    add card style file

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,24 @@
-import Article from '../components/Article'
+import { Link } from 'react-router-dom'
 import { planets } from '../Model'
 
 function Home() {
   return (
     <main>
-      <Article content={planets[0]} />
+      <ul className="card-list | mt-2">
+        {planets.map(({ name, img }) => (
+          <li key={name} className={`card | b-shadow planet_${name}`}>
+            <Link to={`/${name}`} className="flex">
+              <div className="card_image">
+                <img src={img} alt={name} />
+              </div>
+
+              <div className="card_body">
+                <h2 className="card_title">{name}</h2>
+              </div>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </main>
   )
 }
