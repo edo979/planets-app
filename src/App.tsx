7e51bfd50d0c7e2b6ceb6996eed4d011399a9a46
edo979 <prezentabilan@gmail.com>
2022-09-27 11:25:39 +0200

    change language finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Outlet, Link, useParams } from 'react-router-dom'
+import { Outlet, Link, useParams, useNavigate } from 'react-router-dom'
 
 import Navigation from './components/Navigation'
 
@@ -9,10 +9,21 @@ import './scss/style.scss'
 
 function App() {
   const [toggleNav, setToggleNav] = useState<Boolean>(false)
-
   const { lang } = useParams()
+  const navigate = useNavigate()
 
   const hideNav = (): void => setToggleNav(false)
+
+  console.log(lang)
+
+  const handleLangChange = (event: any) => {
+    const lang = event.target.value
+    if (lang !== 'en') {
+      navigate(`/lang/${lang}`)
+    } else {
+      navigate('/')
+    }
+  }
 
   return (
     <>
@@ -35,7 +46,12 @@ function App() {
 
       <footer className="flex">
         <div className="language | skew-box">
-          <select name="lan" id="lang">
+          <select
+            name="lan"
+            id="lang"
+            onChange={handleLangChange}
+            defaultValue={lang ? lang : 'en'}
+          >
             <option value="en">English</option>
             <option value="bs">Bosanski</option>
           </select>
