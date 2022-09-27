84862d0786a4f2ec34cd32b628eeeee3595b4d32
edo979 <prezentabilan@gmail.com>
2022-09-27 10:33:04 +0200

    translate text on the home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import { useState } from 'react'
 import { Outlet, Link, useParams } from 'react-router-dom'
+
 import Navigation from './components/Navigation'
+
+import { getTranslate } from './LangData'
+
 import './scss/style.scss'
 
 function App() {
@@ -14,7 +18,9 @@ function App() {
     <>
       <header className="flex">
         <h1>
-          <Link to={lang ? `/lang/${lang}/` : '/'}>Planets</Link>
+          <Link to={lang ? `/lang/${lang}/` : '/'}>
+            {getTranslate(lang, 'Planets')}
+          </Link>
         </h1>
         <nav className="navbar">
           <Navigation isVisible={toggleNav} hideNav={hideNav} />
@@ -28,11 +34,11 @@ function App() {
       <Outlet />
 
       <footer>
-        Photo by{' '}
+        {`${getTranslate(lang, 'Photo by')}: `}
         <a href="https://unsplash.com/@nasa?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
           NASA
         </a>{' '}
-        on{' '}
+        {`${getTranslate(lang, 'on')}: `}
         <a href="https://unsplash.com/@nasa?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
           Unsplash
         </a>
---
+++ src/LangData.ts
@@ -0,0 +1,20 @@
+const bs: Array<[string, string]> = [
+  ['Planets', 'Planete'],
+  ['Photo by', 'Fotografija od'],
+  ['on', 'od'],
+]
+
+export const getTranslate = (
+  lang: string | undefined,
+  text: string
+): string => {
+  let returnText: [string, string] | undefined
+
+  if (lang === 'bs') {
+    returnText = bs.find((t) => t[0] === text)
+  }
+
+  if (returnText) return returnText[1]
+
+  return text
+}
